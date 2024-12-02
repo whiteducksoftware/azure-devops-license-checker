d8106c0595d8eee277d1e223867746ebe5c803cc
Fabian Spataru <fabian.spataru@whiteduck.de>
2024-12-02 15:54:20 +0100

    refactor: changed method names and api version

--- src/AzureDevOpsLicenseChecker/Cmd/List/ListCommand.cs
+++ src/AzureDevOpsLicenseChecker/Cmd/List/ListCommand.cs
@@ -5,18 +5,18 @@ namespace AzureDevOpsLicenseChecker.Cmd.List;
 
 public class ListCommand
 {
-    public static async Task listAsync(string tenant, string org, string pat)
+    public static async Task ListAsync(string tenant, string org, string pat)
     {
         var table = new Table();
         table.BorderColor(Color.Cyan1);
         table.Border(TableBorder.Rounded);
-        string[] columns = [ "Id", "Account License Type", "MSDN License Type", "Status", "User", "Last Accessed" ];
+        string[] columns = ["Id", "Account License Type", "MSDN License Type", "Status", "User", "Last Accessed"];
         table.AddColumns(columns);
 
         var licenseCheckerService = new LicenseCheckerService(tenant, org, pat);
-        var users = await licenseCheckerService.GetUsers();
+        var users = await licenseCheckerService.GetUsersAsync();
 
-        foreach(var entry in users)
+        foreach (var entry in users)
         {
             table.AddRow(entry.Key,
                          entry.Value.AccessLevel.AccountLicenseType,
